f87fce2ea82c7e7bafd45d4e0e55e8405a5dca8f
dannyandres <danny.aguilar@usach.cl>
2017-04-19 17:56:52 -0300

    readme

--- Lions/resources/views/welcome.blade.php
+++ Lions/resources/views/welcome.blade.php
@@ -5,10 +5,13 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">
+                    <h1>Sientes que no entiendes la materia?</h1>
+                </div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    este sitio web te ayudara en ponerte en contacto con ayudantes capacitados para
+                    aprender mas.
                 </div>
             </div>
         </div>
